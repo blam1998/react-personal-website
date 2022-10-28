e474a40376c66dc3232f0aa71ccedc9eab408e10
blam1998 <57881360+blam1998@users.noreply.github.com>
2022-10-27 20:55:16 -0700

    Added Resume to User information. TODO: Add pop-up for User Information

--- src/user.js
+++ src/user.js
@@ -1,23 +1,51 @@
 import './user.css';
+import {Component} from 'react';
 
 
-function user(){
+function Resume(){
     return(
-        <div className = "Intro">
-            <div className = "UserName">
-                Bao Lam Le H.
+        <div>TEST</div>
+    )
+}
+
+class user extends Component{
+    constructor(props){
+        super(props);
+        this.state = {isToggleOn: true};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(prevState =>({
+            isToggleOn: !prevState.isToggleOn
+        }));
+        
+        if (this.isToggleOn){
+            <div><Resume/></div>
+        }
+        
+
+    }
+
+    render(){
+        return(
+            <div className = "Intro">
+                <div className = "UserName">
+                    Bao Lam Le H.
+                </div>
+                <div className = "UserInfo">
+                    <nobr>
+                        <a  href = "https://github.com/blam1998" className = "fa fa-github">
+                        </a>
+                        <a  href = "https://www.linkedin.com/in/bao-lam-le-431175225/details/projects/" className = "fa fa-linkedin-square">
+                        </a>
+                        <a className = "UserResume" href = "#" onClick = {this.handleClick}>Resume</a>
+
+                    </nobr>
+                </div>
             </div>
-            <div className = "UserInfo">
-                <nobr>
-                    <a className = "UserGithub" href = "https://github.com/blam1998" class = "fa fa-github">
-                    </a>
-                    <a className = "UserLinkedin" href = "https://www.linkedin.com/in/bao-lam-le-431175225/details/projects/" class = "fa fa-linkedin-square">
-                    </a>
-                    <a className = "UserResume" href = "#">Resume</a>
-                </nobr>
-            </div>
-        </div>
-        )
+            )
+    }
 }
 
 export default user;
